7d84f195306afc7f7a32fc6925b9779f44a61060
winniekariuki <winniekariuki07@gmail.com>
2018-11-12 22:57:48 +0300

    modify sales endpoints

--- app/tests/v2/test_app.py
+++ app/tests/v2/test_app.py
@@ -84,7 +84,7 @@ class TestProducts(unittest.TestCase):
         
         user = json.dumps({
             "username": "Eliud",
-            "email":"eliud19@gmail.com",
+            "email":"eliud25@gmail.com",
             "password": "Bb#6060",
             "role": "storeattendant"
         })
@@ -94,7 +94,7 @@ class TestProducts(unittest.TestCase):
         self.assertEqual(response.status_code,201)
     def test_create_product(self):
         product_data = json.dumps({
-            "name": "itel19",
+            "name": "itel25",
             "category":"mobile",
             "price": 2563,
             "quantity":2,
@@ -108,7 +108,7 @@ class TestProducts(unittest.TestCase):
    
     def test_login(self):
         login = json.dumps({
-            "email":"eliud19@gmail.com",
+            "email":"eliud25@gmail.com",
             "password": "Bb#6060"
         })
         response = self.test_client.post(
